2e2fd7837e6945e656963696b501a10f680ba8a6
Chirag Vasant Patil <70964754+chirag4242@users.noreply.github.com>
2023-06-08 02:25:38 -0600

    Remove extra displaylist function

--- script.js
+++ script.js
@@ -60,36 +60,3 @@ function displayList() {
         output.appendChild(listItem);
     }
 }
-
-function displayList() {
-    output.innerHTML = '';
-    for (let i = 0; i < toDoList.length; i++) {
-        const li = document.createElement('li');
-        const taskText = document.createElement('span');
-        const btn = document.createElement('button');
-        btn.innerHTML = `<i class="material-icons">delete</i>`;
-        taskText.textContent = `${toDoList[i]}`
-        li.appendChild(taskText)
-        li.appendChild(btn);
-
-        taskText.addEventListener('dblclick', () => {
-            taskText.contentEditable = true;
-            li.focus();
-        });
-
-        taskText.addEventListener('keydown', (event) => {
-            if (event.key == 'Enter') {
-                event.preventDefault();
-                taskText.contentEditable = false;
-                toDoList[i] = taskText.textContent.trim();
-            }
-        })
-        btn.addEventListener('click', () => {
-            toDoList.splice(i, 1);
-            displayList();
-        })
-        output.appendChild(li);
-
-    }
-}
-
